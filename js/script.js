83342c7b2956baf3f9aee81135a3603d6581cc94
aftersuccess <send.to.avelino@gmail.com>
2024-06-17 17:07:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿const sideNav = document.getElementById("js_SideNav");
+const sideNavBackground = document.getElementById("js_SideNavBackground");
+
+function OpenNav() {
+
+    const container = document.querySelector('.container');
+
+    if (container.clientWidth >= container.clientHeight) {
+        sideNav.style.width = "300px";
+    }
+    else {
+        sideNav.style.width = "250px";
+    }
+
+    sideNavBackground.style.display = 'block';
+
+}
+
+function CloseNav() {
+
+    sideNav.style.width = "0";
+    sideNavBackground.style.display = 'none';
+
+}
